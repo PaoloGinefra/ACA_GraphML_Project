e7ac978db9f0d57cbd22c2bab05f92ea7da921a8
Paolo Ginefra <ginefra.paolo@gmail.com>
2025-06-22 14:43:54 +0200

    chore: improve plotting

--- src/ACAgraphML/Plotting/__init__.py
+++ src/ACAgraphML/Plotting/__init__.py
@@ -1,29 +1,33 @@
 import networkx as nx
 from torch_geometric.utils import to_networkx
-import numpy as np
 import matplotlib.pyplot as plt
-from torch_geometric.data import InMemoryDataset
-import torch
 
 
-def plotGraph(graph, minNodeFeat=0, maxNodeFeat=20, minEdgeFeat=1, maxEdgeFeat=3):
+def plotGraph(graph, title='', minNodeFeat=0, maxNodeFeat=20, minEdgeFeat=1, maxEdgeFeat=3, ax: plt.Axes = None):
     G = to_networkx(graph, to_undirected=True)
-    pos = nx.spring_layout(G, seed=42, weight=graph.edge_attr)
+    pos = nx.spring_layout(
+        G, seed=42, weight=graph.edge_attr*1000, scale=2, k=2, iterations=10000)
+    # Use the provided ax or create a new figure and axis
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(6, 6))
+        show_plot = True
+    else:
+        show_plot = False
 
-    plt.figure(figsize=(6, 6))
     edge_labels = {
         (u, v): f"{graph.edge_attr[i].item()}" for i, (u, v) in enumerate(G.edges)}
     edge_colors = [graph.edge_attr[i].item() for i in range(len(G.edges))]
     nx.draw_networkx_edges(G, pos, edge_color=edge_colors,
-                           edge_cmap=plt.cm.cool, edge_vmin=minEdgeFeat, edge_vmax=maxEdgeFeat)
-    nx.draw_networkx_edge_labels(
-        G, pos, edge_labels=edge_labels, font_color='black', font_size=8)
+                           edge_cmap=plt.cm.cool, edge_vmin=minEdgeFeat, edge_vmax=maxEdgeFeat, ax=ax)
+    # nx.draw_networkx_edge_labels(
+    #     G, pos, edge_labels=edge_labels, font_color='black', font_size=8, ax=ax)
     nx.draw_networkx_nodes(G, pos, node_size=200, node_color=graph.x,
-                           cmap=plt.cm.viridis, vmin=minNodeFeat, vmax=maxNodeFeat)
+                           cmap=plt.cm.tab20, vmin=minNodeFeat, vmax=maxNodeFeat, ax=ax)
 
     labels = {i: str(graph.x[i].item()) for i in G.nodes}
     nx.draw_networkx_labels(G, pos, labels=labels,
-                            font_size=8, font_color='white')
-    plt.title('Graph Visualization')
-    plt.axis('off')
-    plt.show()
+                            font_size=8, font_color='white', ax=ax)
+    ax.set_title(title)
+    ax.axis('off')
+    if show_plot:
+        plt.show()

58f1a3a35457f2456413e3287647c34e9e5b3bc7
Paolo Ginefra <ginefra.paolo@gmail.com>
2025-06-28 11:47:38 +0200

    feat: One hot feat transform

--- src/ACAgraphML/Transforms/__init__.py
+++ src/ACAgraphML/Transforms/__init__.py
@@ -1,3 +1,4 @@
 from .SteadyStateTransform import SteadyStateTransform
 from .OneHotEncodeFeat import OneHotEncodeFeat
+from .OneHotEdgeFeats import OneHotEdgeFeats
 from .BagOfPaths import BagOfPathsTransform
